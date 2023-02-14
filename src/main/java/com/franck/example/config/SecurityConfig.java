778255533219a9ca39a4ea597fc5bc50bfed34e2
franck tchuitio <francktchuitio@MacBook-Air-de-franck.local>
2023-02-14 05:07:04 +0100

    implemented security config class

---
+++ src/main/java/com/franck/example/config/SecurityConfig.java
@@ -0,0 +1,64 @@
+package com.franck.example.config;
+
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.filter.CorsFilter;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class SecurityConfig {
+
+
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity.csrf().disable()
+                .authorizeRequests(
+                        (request) ->
+                                request.antMatchers("/**/auth", "/**/register")
+                        .permitAll()
+                        .anyRequest()
+                        .authenticated()
+                )
+        .authenticationProvider(authenticationProvider())
+        .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class)
+        .cors()
+
+
+        ;
+
+        return httpSecurity.build();
+
+    }
+
+    @Bean
+    public CorsFilter corsFilter() {
+        //todo to be implemented
+        return null;
+    }
+
+
+    @Bean
+    public AuthenticationProvider authenticationProvider() {
+        DaoAuthenticationProvider authenticationProvider = new DaoAuthenticationProvider();
+        authenticationProvider.setUserDetailsService(userDetailsService);
+        authenticationProvider.setPasswordEncoder(passwordEncoder());
+        return authenticationProvider;
+    }
+
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return null;
+    }
+
+
+}
